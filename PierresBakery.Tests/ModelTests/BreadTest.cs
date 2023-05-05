4e5c9d54b34728d9f34a4293e44dfe3ddf0331c1
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-05 09:24:08 -0700

    add passing test, code for the cost of 1 bread loaf

--- PierresBakery.Tests/ModelTests/BreadTest.cs
+++ PierresBakery.Tests/ModelTests/BreadTest.cs
@@ -9,10 +9,21 @@ namespace PierresBakery.Tests
   public class BreadTests
   {
     [TestMethod]
-    public void BreadConstructor_CreatesInstanceofBread_Bread()
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
       Bread newBread = new Bread();
       Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+
+    [TestMethod]
+    public void GetBreadCost_ReturnsCostOfSingleLoaf_BreadCost()  //test for an order of 1 bread loaf
+    {
+      // Arrange
+      Bread newBread = new Bread();
+      // Act
+      int newBreadCost = newBread.GetBreadCost(1);
+      // Assert
+      Assert.AreEqual(5, newBreadCost);
     }
   }
 }
--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -6,6 +6,11 @@ namespace PierresBakery.Models
 {
   public class Bread
   {
-    //properties, constructor
+    public int GetBreadCost(int numberOfLoaves)
+    {
+      int costOfBreadOrder = 0; //initial cost of the order
+      costOfBreadOrder = numberOfLoaves*5; //start with each loaf costing 5
+      return costOfBreadOrder;
+    }
   }
 }
