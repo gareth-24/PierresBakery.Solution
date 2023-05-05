35177e852b0cbfdc451973072657296431d0222e
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-05 15:14:12 -0700

    WIP: try/catch block not throwing the correct error message.

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using PierresBakery.Models;
 
-
 namespace PierresBakery
 {
   public class Program
@@ -16,18 +15,29 @@ namespace PierresBakery
       Console.WriteLine("Bread costs $5 each with a deal of buy 2 get 1 free.");
       Console.WriteLine("Pastries cost $2 each with a deal of buy 3 get 1 free.");
       Console.WriteLine("Enter the number of Bread loaves you would like to buy:");
+
       int breadInput = int.Parse(Console.ReadLine());
       Console.WriteLine("Enter the number of Pastries you would like to buy:");
       int pastryInput = int.Parse(Console.ReadLine());
+      try
+      {
+        Bread newBread = new Bread(breadInput);
+        Pastry newPastry = new Pastry(pastryInput);
+        int breadCost = newBread.GetBreadCost();
+        int pastryCost = newPastry.GetPastryCost();
+        int totalCost = newBread.GetOrderTotal(breadCost, pastryCost);
 
-      Bread newBread = new Bread(breadInput);
-      Pastry newPastry = new Pastry(pastryInput);
-      int breadCost = newBread.GetBreadCost();
-      int pastryCost = newPastry.GetPastryCost();
-      int totalCost = newBread.GetOrderTotal(breadCost, pastryCost);
+        Console.WriteLine($"The total for your order of {breadInput} bread and {pastryInput} pastries is:");
+        Console.WriteLine($"${totalCost}");
+      }
+      catch(Exception invalidNumberInput) //WIP: console freezing on catch block
+      {
+        Console.WriteLine("!!! Error !!!");
+        Console.WriteLine(invalidNumberInput.Message);
+        Console.WriteLine("please enter a valid positive number.");
+        Main();
+      }
 
-      Console.WriteLine($"The total for your order of {breadInput} bread and {pastryInput} pastries is:");
-      Console.WriteLine($"${totalCost}");
 
       Console.WriteLine("Would you like to reset your cart and make another order? (yes/no)");
       string response = Console.ReadLine().ToLower();
@@ -43,28 +53,3 @@ namespace PierresBakery
     }
   }
 }
-
-//       try
-//       {
-//         DateTime date = new DateTime(year, month, day);
-//         Calendar newCalendar = CultureInfo.InvariantCulture.Calendar;
-//         Console.WriteLine(newCalendar.GetDayOfWeek(date));
-//       }
-//       catch(Exception invalidDate)
-//       {
-//         Console.WriteLine(invalidDate.Message);
-//         Console.WriteLine("Please enter a date between 01/01/0001 and 12/31/9999");
-//       }
-//       Console.WriteLine("Would you like to check another date? (yes/no)");
-//       string response = Console.ReadLine();
-//       if (response == "yes")
-//       {
-//         Main();
-//       }
-//       else
-//       {
-//         Console.WriteLine("Goodbye");
-//       }
-//     }
-//   }
-// }
