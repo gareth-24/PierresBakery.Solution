f76fd048d6493a3ac57d388080c31d746e0af01a
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-05 13:12:37 -0700

    add passing test, code for NumOfPastries get functionality.

--- PierresBakery.Tests/ModelTests/PastryTest.cs
+++ PierresBakery.Tests/ModelTests/PastryTest.cs
@@ -11,7 +11,7 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void PastryConstructor_CreatesInstanceofPastry_Pastry()
     {
-      Pastry newPastry = new Pastry();
+      Pastry newPastry = new Pastry(1);
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
 
@@ -19,7 +19,7 @@ namespace PierresBakery.Tests
     public void GetPastryCost_ReturnsCostOfSinglePastry_PastryCost()  //test for an order of 1 pastry
     {
       // Arrange
-      Pastry newPastry = new Pastry();
+      Pastry newPastry = new Pastry(1);
       // Act
       int newPastryCost = newPastry.GetPastryCost(1);
       // Assert
@@ -30,11 +30,23 @@ namespace PierresBakery.Tests
     public void GetPastryCost_ReturnsCostOfMultiplePastries_PastryCost()  //test for an order of many pastries
     {
       // Arrange
-      Pastry newPastry = new Pastry();
+      Pastry newPastry = new Pastry(7);
       // Act
       int newPastryCost = newPastry.GetPastryCost(7);
       // Assert
       Assert.AreEqual(12, newPastryCost); //buying 7, get 1 pastry free
+    }
+
+    [TestMethod]
+    public void GetNumOfPastries_ReturnsNumOfPastries_Int()
+    {
+      // Arrange
+      int amountOfPastries = 4;
+      Pastry newPastry = new Pastry(amountOfPastries);
+      // Act
+      int result = newPastry.NumOfPastries;
+      // Assert
+      Assert.AreEqual(amountOfPastries, result); // test that the auto-implemented property works with get
     }
   }
 }
--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -6,6 +6,11 @@ namespace PierresBakery.Models
 {
   public class Pastry
   {
+    public int NumOfPastries {get; set; }
+    public Pastry(int amountOfPastries)
+    {
+      NumOfPastries = amountOfPastries;
+    }
     public int GetPastryCost(int numberOfPastries)
     {
       int costOfPastryOrder = 0;  // initial cost of the order
